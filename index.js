9c05860f6813a9ad7e483e043c7edc9f046e94aa
gogogabe9000 <gabe.email1032@yahoo.com>
2022-01-23 20:22:10 -0500

    debugging left path

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
+    if (secondAnswer === 'follow') {
+        let thirdAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
+
+     } if (thirdAnswer === 'stay') {
+            window.alert('You live happily amongst the cats for the rest of your days.')
+        } else if (thirdAnswer === 'spread the word') {
+            window.alert('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
+        }
+    
 
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
